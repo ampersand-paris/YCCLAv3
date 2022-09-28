091774827584064a20e699517c583d9764ee51a5
ampersand-paris <ampersand.s.paris@gmail.com>
2022-09-28 11:02:45 -0400

    media configuration

--- category/models.py
+++ category/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+# Currently this model is used as a stand-in for postcard collection aka 'Summer Series', 'Pickle Pack', etc.
+# Perhaps there is also a place to add Recipe Author and Illustrator in the model?
+
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    recipe_authors = models.CharField(max_length=100)
+    illustrators = models.CharField(max_length=100)
     description = models.TextField(max_length=255, blank=True)
     category_image = models.ImageField(upload_to='photos/categories', blank=True)
 
--- ycclav3/settings.py
+++ ycclav3/settings.py
@@ -130,11 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Static files config
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'ycclav3/static',
 ]
+
+# Media files config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
